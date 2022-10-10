58a867a525a6d60459e76b8a277a527c45d692f5
Ji Park <jiyongprk@gmail.com>
2022-10-10 17:34:24 -0400

    finish all logics, work on styling now

--- src/components/DateDisplay.js
+++ src/components/DateDisplay.js
@@ -3,8 +3,11 @@ import React from 'react'
 export default function DateDisplay({ dates }) {
 
     const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+    const days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
 
     const newDateFormat = new Date(dates.date)
+    // Text Day Method : Monday,Tuesday...
+    // let day = days[newDateFormat.getDay()]
     let day = newDateFormat.getDay()
     let month = months[newDateFormat.getMonth()]
     let year = newDateFormat.getFullYear()
@@ -12,9 +15,9 @@ export default function DateDisplay({ dates }) {
     return (
         <>
             <ul style={{ listStyleType: 'none' }}>
-                <li>Day: {day} </li>
+                <li>Day: {!day ? null : day} </li>
                 <li>Month: {month}</li>
-                <li>Year: {year}</li>
+                <li>Year: {!year ? null : year}</li>
             </ul>
         </>
     )
